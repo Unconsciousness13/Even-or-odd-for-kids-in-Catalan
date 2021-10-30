8b4b8b0369593be51725a93ba360a2f408ca1b54
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-10-30 15:42:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function calculate() {
+    const inputValue = document.querySelector("#number");
+    const resultValue = document.getElementById("result");
+    const resultButton = document.getElementById("buttCalc");
+    resultButton.addEventListener("click", calcule);
+
+    function calcule(e) {
+        e.preventDefault();
+        let result = '';
+        if (!inputValue.value == "" || !inputValue.value === "string") {
+            if (inputValue.value % 2 === 0) {
+                result = `El número ${inputValue.value} és parell
+                `
+            } else if (inputValue.value % 2 === 1) {
+                result = `El número ${inputValue.value} és senar`
+            } else {
+                result = `No és número !`
+            }
+
+        }
+        inputValue.value = ''
+        resultValue.textContent = result;
+    }
+}
